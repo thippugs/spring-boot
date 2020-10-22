4149b4f1643293489b10b298c5c91247fd28c0b8
thippugs <tippu_gs@yahoo.com>
2020-10-22 18:53:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+  access_key=""
+  secret_key=""
+  region="us-east-1"
+}
 
+terraform {
+  backend "s3"{
+    bucket = "thippu-tj-bucket"
+    key = "retestbucket"
+    region="us-east-1"
+  }
+
+}
